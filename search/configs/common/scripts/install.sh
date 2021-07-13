3064e931f0d77fa196a4a968db9de9cfa8a226ae
ben <ben.niu@liferay.com>
2021-07-13 12:16:01 +0900

    check if the file version works.

--- search/configs/common/scripts/install.sh
+++ search/configs/common/scripts/install.sh
@@ -1,3 +1,6 @@
+#Let’s have A before and after
+echo Before the install.
+bin/elasticsearch-plugin list
 echo installing neologd-7.9.3 from install.sh...
-elasticsearch-plugin install file:/usr/share/elasticsearch/downloadedfile/elasticsearch-analysis-kuromoji-ipadic-neologd-7.9.3-SNAPSHOT.zip
+#elasticsearch-plugin install file:/usr/share/elasticsearch/downloadedfile/elasticsearch-analysis-kuromoji-ipadic-neologd-7.9.3-SNAPSHOT.zip
 bin/elasticsearch-plugin list

eb0554dab025892c86f0a0250ca6b9417ccf6216
ben <ben.niu@liferay.com>
2021-07-13 16:42:43 +0900

    Added console log.

--- search/configs/common/scripts/install.sh
+++ search/configs/common/scripts/install.sh
@@ -1,6 +1,7 @@
 #Let’s have A before and after
-echo Before the install.
+echo [install.sh] Before the install.
 bin/elasticsearch-plugin list
-echo installing neologd-7.9.3 from install.sh...
-#elasticsearch-plugin install file:/usr/share/elasticsearch/downloadedfile/elasticsearch-analysis-kuromoji-ipadic-neologd-7.9.3-SNAPSHOT.zip
+echo [install.sh] Now installing neologd-7.9.3 from install.sh...
+elasticsearch-plugin install file:/usr/share/elasticsearch/downloadedfile/elasticsearch-analysis-kuromoji-ipadic-neologd-7.9.3-SNAPSHOT.zip
+echo [install.sh] After installing neologd-7.9.3 from install.sh...
 bin/elasticsearch-plugin list
